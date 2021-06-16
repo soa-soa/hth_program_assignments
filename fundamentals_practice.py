848db5e8e7dec66f0d3d005ceb781c8bee4ca2d7
soa-soa <you@example.com>
2021-06-16 09:39:20 -0700

    declared new variables

--- fundamentals_practice.py
+++ fundamentals_practice.py
@@ -1,8 +1,22 @@
-print("Hello World")
-name = "lebron"
-age = 35
-salary = 37.44
-plays_basketball = True
-jersey_number = "23"
-#python fundamentals_practice.py 
+print("Hello World") 
+# python fundamentals_practice.py 
+
+# step 1
+name = "lebron" # declaring name variable
+age = 35 # declared age var
+salary = 37.44 # declared salary var
+plays_basketball = True # declared boolean var
+jersey_number = "23" # declared jersey number
+
+# step 2
+car_name = "Ford"
+x = 50
+my_first_name = "John" # numbers can't be in front, no dashes (means minus)
+
+# step 3
+print(type(name))
+print(type(age))
+print(type(salary))
+print(type(plays_basketball))
+print(type(jersey_number))
 
